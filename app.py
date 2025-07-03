7b893244129a31ed9befe7f6e136a037312dad0a
kevinphan305 <kenfedit@gmail.com>
2025-07-03 09:21:37 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from binance.client import Client
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_SECRET_KEY")
+
+client = Client(api_key, api_secret)
+
+def get_price(symbol="BTCUSDT"):
+    ticker = client.get_symbol_ticker(symbol=symbol)
+    return ticker
+
+if __name__ == "__main__":
+    print(get_price())
